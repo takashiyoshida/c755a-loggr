3bab3a4382f58e757b2884f30d6ac2bc7b1eadea
Takashi Yoshida <takashi@takashiyoshida.org>
2015-02-23 23:27:16 +0800

    Add Trip class

--- tmc/timetable.py
+++ tmc/timetable.py
@@ -19,3 +19,31 @@ class StopPoint:
         
     def __repr__(self):
         return "StopPoint: %s %s %s %s %s" % (self._name, self._dwell, self._runtime, self._arrival, self._departure)
+
+class Trip:
+    def __init__(self, trip, schedule, origin, destination, direction, departure, before, after, numOfStops):
+        self._trip = trip
+        self._schedule = schedule
+        self._origin = origin
+        self._destination = destination
+        self._direction = int(direction)
+        self._departure = datetime.strptime(departure, "%H:%M:%S")
+        self._before = before
+        self._after = after
+        self._numOfStops = int(numOfStops)
+        self._stopPointList = []
+        
+    def addStopPoint(self, stopPoint):
+        self._stopPointList.append(stopPoint)
+        
+    def shiftTime(self, delta):
+        self._departure += delta
+        for stopPoint in self._stopPointList:
+            stopPoint.shiftTime(delta)
+            
+    def __repr__(self):
+        description = "Trip: %s %s %s %s %d %s %s %s %d" % (self._trip, self._schedule, self._origin, self._destination, self._direction, self._departure, self._before, self._after, self._numOfStops)
+        
+        for stopPoint in self._stopPointList:
+            description += "\n%s" % (stopPoint)
+        return description
