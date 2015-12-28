82bfc46658f618d3881a1e6b06a533201cab44e5
t0062248 <takashi.yoshida@asia.thalesgroup.com>
2015-12-28 15:29:53 +0800

    Use constant rather than hardcoded values

--- rad/event.py
+++ rad/event.py
@@ -100,80 +100,150 @@ class RadLogEvent:
         0x8343: "SC Active Alarm",
     }
 
+    CCTV_CARRIER_ON = 0x01
+    CCTV_CARRIER_OFF = 0x02
+    CCTV_QUAD_SCREEN = 0x03
+    CCTV_SINGLE_SCREEN = 0x04
+    CCTV_SEQUENCE = 0x05
+    CCTV_RETURN_TO_DEFAULT = 0x06
+    CCTV_COMMAND_RECEIVED = 0x07
+    CCTV_FREQUENCY_CHANGE = 0x08
+
+    PA_LIVE_ANNOUNCEMENT = 0x0b
+    PA_PRE_RECORDED_ANNOUCEMENT = 0x0c
+    PA_DVA_ANNOUCEMENT = 0x0d
+    PA_RESET = 0x0e
+    PA_REQUEST_ATAS_VERSION = 0x0f
+    PA_ATAS_LIBRARY_ENABLE = 0x10
+    PA_ATAS_LIBRARY_DISABLE = 0x11
+    PA_CONTINUE = 0x12
+    PA_COMMAND_RECEIVED = 0x13
+    PA_READY_FOR_LIVE_DVA_ANNOUCEMENT = 0x14
+    PA_ATAS_VERSION_NUMBER = 0x15
+    PA_REQUEST_FOR_PA_RESET = 0x16
+    PA_AUDIO_SWITCH_ON_PA = 0x17
+    PA_TRAIN_PA_MESSAGE_COMPLETED = 0x18
+    PA_ATAS_CYCLIC_ANNOUNCEMENT = 0x19
+    PA_AUDIO_SWITCH_ON_CAB_TO_CAB = 0x1a
+
+    PIS_FREE_TEXT_MESSAGE = 0x1f
+    PIS_PRE_STORED_MESSAGE = 0x20
+    PIS_LIBRARY_DOWNLOAD = 0x21
+    PIS_LIBRARY_UPLOAD = 0x22
+    PIS_RESET_EMERGENCY_MESSAGE = 0x23
+    PIS_REQUEST_FOR_PIS_VERSION = 0x24
+    PIS_LIBRARY_ENABLE = 0x25
+    PIS_LIBRARY_DISABLE = 0x26
+    PIS_END_OF_PIS_DOWNLOAD = 0x27
+    PIS_VERSION_NUMBER = 0x29
+    PIS_COMMAND_RECEIVED = 0x2a
+    PIS_LIBRARY_UPGRADE = 0x2b
+    PIS_SCHEDULE_DOWNLOAD = 0x2c
+    PIS_SCHEDULE_UPGRADE = 0x2d
+
+    PEC_ANSWER = 0x33
+    PEC_RESET = 0x34
+    PEC_ACTIVATED = 0x35
+    PEC_SELECTED_BY_DRIVER = 0x36
+    PEC_COMMAND_RECEIVED = 0x37
+    PEC_READY_FOR_PEC_CONVERSATION = 0x38
+    PEC_REQUEST_FOR_PEC_RESET = 0x39
+    PEC_CONTINUE = 0x3a
+
+    CRITICAL_ALARM = 0x3d
+    REQUEST_FOR_OCC_CALL = 0x3f
+    REQUEST_FOR_COMM_CHANGEOVER = 0x40
+    CHANGEOVER_STATUS = 0x41
+    BAD_COMMAND = 0x42
+    CHANGEOVER_STARTED = 0x43
+    OCC_CALL_COMMAND_RECEIVED = 0x44
+    OCC_CALL_RESET = 0x45
+    END_OF_OCC_CALL = 0x46
+    TEST_CALL = 0x47
+    TEST_CALL_RESULT = 0x48
+    TETRA_ISCS_MODE = 0x49
+    REQUEST_FOR_VOICE_CALL = 0x4a
+    CHANGE_AREA = 0x4b
+    TETRA_ISCS_MODE_RECEIVED = 0x4c
+    CHANGE_AREA_RECEIVED = 0x4d
+    VOICE_CALL_COMMAND_RECEIVED = 0x4e
+    END_OF_VOICE_CALL = 0x4f
+    TEST_ALARM = 0x5a
+    TCI_ALARMS = 0x5b
+
     _command_identifiers = {
         # CCTV
-        0x1: "Carrier On",
-        0x2: "Carrier Off",
-        0x3: "Quad Screen",
-        0x4: "Single Screen",
-        0x5: "Sequence",
-        0x6: "Return to Default",
-        0x9: "Command Received",
-        0xa: "Frequency Change",
+        CCTV_CARRIER_ON: "Carrier On",
+        CCTV_CARRIER_OFF: "Carrier Off",
+        CCTV_QUAD_SCREEN: "Quad Screen",
+        CCTV_SINGLE_SCREEN: "Single Screen",
+        CCTV_SEQUENCE: "Sequence",
+        CCTV_RETURN_TO_DEFAULT: "Return to Default",
+        CCTV_COMMAND_RECEIVED: "CCTV Command Received",
+        CCTV_FREQUENCY_CHANGE: "Frequency Change",
         # Public Announcement
-        0xb: "PA Live Announcement",
-        0xc: "Pre-recorded Annoucement",
-        0xd: "DVA Announcement",
-        0xe: "PA Reset",
-        0xf: "Request for ATAS Version (1)",
-        0x10: "ATAS Library Enable (1)",
-        0x11: "ATAS Library Disable (1)",
-        0x12: "PA Continue",
-        0x13: "PA Command Received",
-        0x14: "Ready for Live or DVA Annoucement",
-        0x15: "ATAS Version Number",
-        0x16: "Request for PA Reset",
-        0x17: "Audio SW on PA",
-        0x18: "Train PA Message Completed",
-        0x19: "ATAS Cyclic Annoucement",
-        0x1a: "Audio SW on Cab to Cab",
+        PA_LIVE_ANNOUNCEMENT: "PA Live Announcement",
+        PA_PRE_RECORDED_ANNOUCEMENT: "Pre-recorded Annoucement",
+        PA_DVA_ANNOUCEMENT: "DVA Announcement",
+        PA_RESET: "PA Reset",
+        PA_REQUEST_ATAS_VERSION: "Request for ATAS Version",
+        PA_ATAS_LIBRARY_ENABLE: "ATAS Library Enable (1)",
+        PA_ATAS_LIBRARY_DISABLE: "ATAS Library Disable (1)",
+        PA_CONTINUE: "PA Continue",
+        PA_COMMAND_RECEIVED: "PA Command Received",
+        PA_READY_FOR_LIVE_DVA_ANNOUCEMENT: "Ready for Live or DVA Annoucement",
+        PA_ATAS_VERSION_NUMBER: "ATAS Version Number",
+        PA_REQUEST_FOR_PA_RESET: "Request for PA Reset",
+        PA_AUDIO_SWITCH_ON_PA: "Audio Switch on PA",
+        PA_TRAIN_PA_MESSAGE_COMPLETED: "Train PA Message Completed",
+        PA_ATAS_CYCLIC_ANNOUNCEMENT: "ATAS Cyclic Annoucement",
+        PA_AUDIO_SWITCH_ON_CAB_TO_CAB: "Audio SW on Cab to Cab",
         # Passenger Infomration System
-        0x1f: "PIS Free-Text Message",
-        0x20: "PIS Pre-Stored Message",
-        0x21: "PIS Library Download",
-        0x22: "PIS Library Upload (1)",
-        0x23: "Reset Emergency Message",
-        0x24: "Request for PIS Version (1)",
-        0x25: "PIS Library Enable (1)",
-        0x26: "PIS Library Disable (1)",
-        0x27: "End of PIS Download",
-        0x28: "Blank",
-        0x29: "PIS Version Number",
-        0x2a: "PIS Command Received",
-        0x2b: "PIS Library Upgrade",
-        0x2c: "PIS Schedule Download",
-        0x2d: "PIS Schedule Upgrade",
+        PIS_FREE_TEXT_MESSAGE: "PIS Free-Text Message",
+        PIS_PRE_STORED_MESSAGE: "PIS Pre-Stored Message",
+        PIS_LIBRARY_DOWNLOAD: "PIS Library Download",
+        PIS_LIBRARY_UPLOAD: "PIS Library Upload",
+        PIS_RESET_EMERGENCY_MESSAGE: "Reset Emergency Message",
+        PIS_REQUEST_FOR_PIS_VERSION: "Request for PIS Version",
+        PIS_LIBRARY_ENABLE: "PIS Library Enable",
+        PIS_LIBRARY_DISABLE: "PIS Library Disable",
+        PIS_END_OF_PIS_DOWNLOAD: "End of PIS Download",
+        PIS_VERSION_NUMBER: "PIS Version Number",
+        PIS_COMMAND_RECEIVED: "PIS Command Received",
+        PIS_LIBRARY_UPGRADE: "PIS Library Upgrade",
+        PIS_SCHEDULE_DOWNLOAD: "PIS Schedule Download",
+        PIS_SCHEDULE_UPGRADE: "PIS Schedule Upgrade",
         # PEC
-        0x33: "PEC Answer",
-        0x34: "PEC Reset",
-        0x35: "PEC Activated",
-        0x36: "PEC Selected By Driver",
-        0x37: "PEC Command Received",
-        0x38: "Ready for PEC Conversation",
-        0x39: "Request for PEC Reset",
-        0x3a: "PEC Continue",
+        PEC_ANSWER: "PEC Answer",
+        PEC_RESET: "PEC Reset",
+        PEC_ACTIVATED: "PEC Activated",
+        PEC_SELECTED_BY_DRIVER: "PEC Selected By Driver",
+        PEC_COMMAND_RECEIVED: "PEC Command Received",
+        PEC_READY_FOR_PEC_CONVERSATION: "Ready for PEC Conversation",
+        PEC_REQUEST_FOR_PEC_RESET: "Request for PEC Reset",
+        PEC_CONTINUE: "PEC Continue",
         # Alarms, Switchover, Calls
-        0x3d: "Critical Alarm",
-        0x3f: "Request for OCC Call",
-        0x40: "Request for Comm Changeover",
-        0x41: "Changeover Status",
-        0x42: "Bad Command",
-        0x43: "Changeover Started",
-        0x44: "OCC Call Command Received",
-        0x45: "OCC Call Reset",
-        0x46: "End of OCC Call",
-        0x47: "Test Call",
-        0x48: "Test Call Result",
-        0x49: "TETRA/ISCS Mode",
-        0x4a: "Request for Voice Call",
+        CRITICAL_ALARM: "Critical Alarm",
+        REQUEST_FOR_OCC_CALL: "Request for OCC Call",
+        REQUEST_FOR_COMM_CHANGEOVER: "Request for Comm Changeover",
+        CHANGEOVER_STATUS: "Changeover Status",
+        BAD_COMMAND: "Bad Command",
+        CHANGEOVER_STARTED: "Changeover Started",
+        OCC_CALL_COMMAND_RECEIVED: "OCC Call Command Received",
+        OCC_CALL_RESET: "OCC Call Reset",
+        END_OF_OCC_CALL: "End of OCC Call",
+        TEST_CALL: "Test Call",
+        TEST_CALL_RESULT: "Test Call Result",
+        TETRA_ISCS_MODE: "TETRA/ISCS Mode",
+        REQUEST_FOR_VOICE_CALL: "Request for Voice Call",
         # Change Area, Voice Call
-        0x4b: "Change Area",
-        0x4c: "TETRA/ISCS Mode Received",
-        0x4d: "Change Area Received",
-        0x4e: "Voice Call Command Received",
-        0x4f: "End of Voice Call",
-        0x5a: "Test Alarm (2) Not for ISCS",
-        0x5b: "TCI Alarms (2) Not for ISCS",
+        CHANGE_AREA: "Change Area",
+        TETRA_ISCS_MODE_RECEIVED: "TETRA/ISCS Mode Received",
+        CHANGE_AREA_RECEIVED: "Change Area Received",
+        VOICE_CALL_COMMAND_RECEIVED: "Voice Call Command Received",
+        END_OF_VOICE_CALL: "End of Voice Call",
+        TEST_ALARM: "Test Alarm",
+        TCI_ALARMS: "TCI Alarms (2) Not for ISCS",
     }
 
     def __init__(self, timestamp, trans_id, status, api_type, api_id, now = datetime.now()):
@@ -219,73 +289,65 @@ class RadLogEvent:
                     self._message = self._command_identifiers[command]
                     self._get_atc_car_num(flag)
 
-                    if command == 0xb:
-                        # PA live announcement
+                    if command == RadLogEvent.PA_LIVE_ANNOUNCEMENT:
                         self._note = "Announcement ID: %d" % (self._data[flag + 7])
-                    elif command == 0xc:
-                        # Pre-recorded announcement
+                    elif command == RadLogEvent.PA_PRE_RECORDED_ANNOUCEMENT:
                         self._note = "Announcement ID: %d" % (self._data[flag + 11])
-                    elif command == 0xd:
-                        # DVA announcement
+                    elif command == RadLogEvent.PA_DVA_ANNOUCEMENT:
                         self._note = "Announcement ID: %d" % (self._data[flag + 7])
-                    elif command == 0xe:
-                        # PA reset
+                    elif command == RadLogEvent.PA_RESET:
                         self._note = "Announcement ID: %d" % (self._data[flag + 6])
-                    elif command == 0x12:
-                        # PA continue
+                    elif command == RadLogEvent.PA_CONTINUE:
                         self._note = "Announcement ID: %d" % (self._data[flag + 6])
-                    elif command == 0x13:
-                        # PA command received
+                    elif command == RadLogEvent.PA_COMMAND_RECEIVED:
                         self._note = "Announcement ID: %d" % (self._data[flag + 11])
-                    elif command == 0x14:
-                        # Ready for live or DVA announcement
+                    elif command == RadLogEvent.PA_READY_FOR_LIVE_DVA_ANNOUCEMENT:
                         self._note = "Announcement ID: %d" % (self._data[flag + 6])
-                    elif command == 0x16:
+                    elif command == RadLogEvent.PA_REQUEST_FOR_PA_RESET:
                         self._note = "Announcement ID: %d" % (self._data[flag + 6])
-                    elif command == 0x18:
+                    elif command == RadLogEvent.PA_TRAIN_PA_MESSAGE_COMPLETED:
                         self._note = "Announcement ID: %d" % (self._data[flag + 7])
-                    elif command == 0x19:
+                    elif command == RadLogEvent.PA_ATAS_CYCLIC_ANNOUNCEMENT:
                         self._note = "Announcement ID: %d" % (self._data[flag + 15])
-                    elif command == 0x1f:
-                        # PIS free text message
+                    elif command == RadLogEvent.PIS_FREE_TEXT_MESSAGE:
                         self._note = "PID Address: %d" % (self._data[flag + 6])
-                    elif command == 0x20:
+                    elif command == RadLogEvent.PIS_PRE_STORED_MESSAGE:
                         # PIS pre-stored message
                         self._note = "PID Address: %d" % (self._data[flag + 6])
-                    elif command == 0x22:
+                    elif command == RadLogEvent.PIS_LIBRARY_UPLOAD:
                         # PIS library upload
                         self._note = "Status 9: %d" % (self._data[flag + 6])
-                    elif command == 0x23:
+                    elif command == RadLogEvent.PIS_RESET_EMERGENCY_MESSAGE:
                         # Reset emergency message
                         self._note = "PID Address: %d" % (self._data[flag + 6])
-                    elif command == 0x27:
+                    elif command == RadLogEvent.PIS_END_OF_PIS_DOWNLOAD:
                         # End of PIS download
                         self._note = "Status 10: %d" % (self._data[flag + 6])
-                    elif command == 0x2a:
+                    elif command == RadLogEvent.PIS_COMMAND_RECEIVED:
                         # PIS command received
                         self._note = "Status 5: %d" % (self._data[flag + 6])
-                    elif command == 0x33:
+                    elif command == RadLogEvent.PEC_ANSWER:
                         # PEC answer
                         self._note = "PEC number: %d" % (self._data[flag + 6])
-                    elif command == 0x34:
+                    elif command == RadLogEvent.PEC_RESET:
                         # PEC reset
                         self._note = "PEC number: %d" % (self._data[flag + 6])
-                    elif command == 0x35:
+                    elif command == RadLogEvent.PEC_ACTIVATED:
                         # PEC activated
                         self._note = "PEC number: %d" % (self._data[flag + 6])
-                    elif command == 0x36:
+                    elif command == RadLogEvent.PEC_SELECTED_BY_DRIVER:
                         # PEC selected by driver
                         self._note = "PEC number: %d" % (self._data[flag + 6])
-                    elif command == 0x37:
+                    elif command == RadLogEvent.PEC_COMMAND_RECEIVED:
                         # PEC command received
                         self._note = "PEC number: %d, Status 6: %d" % (self._data[flag + 6], self._data[flag + 7])
-                    elif command == 0x38:
+                    elif command == RadLogEvent.PEC_READY_FOR_PEC_CONVERSATION:
                         # Ready for PEC conversation
                         self._note = "PEC number: %d" % (self._data[flag + 6])
-                    elif command == 0x39:
+                    elif command == RadLogEvent.PEC_REQUEST_FOR_PEC_RESET:
                         # Request for PEC reset
                         self._note = "PEC number: %d" % (self._data[flag + 6])
-                    elif command == 0x3a:
+                    elif command == RadLogEvent.PEC_CONTINUE:
                         # PEC continue
                         self._note = "PEC number: %d" % (self._data[flag + 6])
                     else:
