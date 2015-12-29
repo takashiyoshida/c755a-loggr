8f52a91e365d01b7a5017a3625183ab47b5e68a6
t0062248 <takashi.yoshida@asia.thalesgroup.com>
2015-12-29 10:21:11 +0800

    Still wondering if event.decode() should be called from parser or not

--- rad/radiogaga.py
+++ rad/radiogaga.py
@@ -5,20 +5,20 @@ import csv
 from rad.parser import RadLogParser
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog = "radiogaga", description = "")
-    parser.add_argument("-l", "--log", required = True, help = "rad_log",
-                        dest = "rad_log")
-    parser.add_argument("-o", "--output", required = True, help = "output", dest = "output")
-    args = parser.parse_args()
-    #print args
+    argparser = argparse.ArgumentParser(prog = "radiogaga", description = "")
+    argparser.add_argument("-l", "--log", required = True, help = "rad_log", dest = "rad_log")
+    argparser.add_argument("-o", "--output", required = True, help = "output", dest = "output")
+    args = argparser.parse_args()
 
-    radEvents = []
-    logParser = RadLogParser()
-    radEvents = logParser.parse_log(args.rad_log)
+    events = []
+    parser = RadLogParser()
+    events = parser.parse_log(args.rad_log)
 
     with open(args.output, 'w') as f:
         fieldnames = ['Timestamp', 'ATC Car No.', 'Transmission ID', 'Status', 'Method/Event', 'Method/Event ID', 'Method/Event Name', 'Message', 'Parameter']
+
         writer = csv.writer(f)
         writer.writerow(fieldnames)
-        for event in radEvents:
+
+        for event in events:
             writer.writerow(event.toCsv())
